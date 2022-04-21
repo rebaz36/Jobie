9d7a735ae90a35260d4d1e7f6500d1ed3eb26c44
rebaz36 <rebaz415@gmail.com>
2022-04-21 05:59:07 +0300

    m

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo " alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link button"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
